9d5f4671368ecf85938303fe76ab9b77ff33f482
vamsi-273006 <tvkrishnacse@gmail.com>
2025-04-29 22:57:52 -0400

    Assigment-2

---
+++ src/assigments/Assignment2.java
@@ -0,0 +1,11 @@
+package assigments;
+
+public class Assignment2 {
+	public static void main(String[] args) {
+		System.out.println("\" \" \" \" \" \" \"");
+		System.out.println("\"   *   *   \"");
+		System.out.println("\"     *     \"");
+		System.out.println("\"   * * *   \"");
+		System.out.println("\" \" \" \" \" \" \"");
+	}
+}
---
+++ src/assigments/CollegeDetails.java
@@ -0,0 +1,10 @@
+package assigments;
+
+public class CollegeDetails {
+	
+	public static void main(String[] args) {
+		System.out.println("My name is Vamsikrishna Talupula");
+		System.out.println("Iam a B-tech student");
+		System.out.println("In sree vahini institute of Science and technology");
+	}
+}
